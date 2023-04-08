5df0aa8b064cd4b21074e4059da93e5f5cf87e79
nithinmv4u <nithinmv4u@gmail.com>
2023-04-08 20:33:56 +0530

    settings db host port add

--- wowshop/settings.py
+++ wowshop/settings.py
@@ -105,8 +105,8 @@ DATABASES = {
         'NAME': env('NAME'),
         'USER': env('USER'),
         'PASSWORD': env('PASSWORD'),
-        # 'HOST': env('HOST'),
-        # 'PORT': env('PORT'),
+        'HOST': env('HOST'),
+        'PORT': env('PORT'),
     }
 }
 
